c623859cd09428da35d398d8b4c28c9b2692aea1
Hedayat-developer <hedayat.dev@parktech.af>
2024-12-15 10:28:35 +0430

    eager loaded images to products

--- app/Http/Controllers/v1/ProductController.php
+++ app/Http/Controllers/v1/ProductController.php
@@ -15,7 +15,8 @@ class ProductController extends Controller
   use FileHandling;
     public function index()
     {
-        return Product::paginate($request->limit ?? 10, ['*'], 'page', $request->page ?? 1);
+        return Product::with('images')->paginate($request->limit ?? 10, ['*'], 'page', $request->page ?? 1);
+
     }
 
     /**
