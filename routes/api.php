7c1d53d598dd34ed9b0f08f617136262cfd044e7
Hedayat-developer <hedayat.dev@parktech.af>
2024-12-16 16:00:29 +0430

    routes commented

--- routes/api.php
+++ routes/api.php
@@ -22,40 +22,40 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
 
     Route::apiResource('customers', CustomerController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Orders routes with permissions
     Route::apiResource('orders', OrderController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Products routes with permissions
     Route::apiResource('products', ProductController::class);
     // Categories routes with permissions
     Route::apiResource('category', ProductCategoryController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Product Images routes with permissions
     Route::apiResource('products-images', ProductImageController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Items routes with permissions
@@ -69,37 +69,37 @@ Route::prefix('v1')->group(function () {
     
     // Product Items routes with permissions
     Route::apiResource('product-items', ProductItemController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Items Addons routes with permissions
     Route::apiResource('items-addons', ItemsAddonController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Expenses routes with permissions
     Route::apiResource('expenses', ExpenseController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
     
     // Expense Categories routes with permissions
     Route::apiResource('expense-categories', ExpenseCategoryController::class)->middleware([
-        'index' => 'permission:view',
-        'show' => 'permission:view',
-        'store' => 'permission:create',
-        'update' => 'permission:update',
-        'destroy' => 'permission:delete',
+        // 'index' => 'permission:view',
+        // 'show' => 'permission:view',
+        // 'store' => 'permission:create',
+        // 'update' => 'permission:update',
+        // 'destroy' => 'permission:delete',
     ]);
 });
